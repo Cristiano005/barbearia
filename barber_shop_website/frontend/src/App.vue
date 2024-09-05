543aef6858945adcfdc1960b0ce68b49aa93899d
Erisvaldo15 <erisvaldosilvadesousa38@gmail.com>
2024-09-05 19:56:17 -0300

    Starting to update the Home Page

---
+++ barber_shop_website/frontend/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
+</script>
+
+<template>
+    <header class="fixed-top">
+        <HeaderComponent />
+    </header>
+    <main>
+        <RouterView />
+    </main>
+    <footer></footer>
+</template>
+
+<style scoped>
+
+header {
+  height: 4.5rem;
+}
+
+main {
+  margin: 4.5rem 0;
+}
+
+section {
+  padding: 10rem 0;
+}
+
+</style>
---
+++ barber_shop_website/frontend/src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+    <section id="home">
+        <div class="d-flex gap-4 mx-auto" id="container-home">
+            <div id="apresentation">
+                <h2 class="lh-base mb-4">Wecolme to Barber Shop in Nova Odessa.</h2>
+                <p class="lh-base mb-4">
+                    Um salão exclusivo em São Paulo, especializado em tratamentos naturais.
+                </p>
+                <button class="d-flex align-items-center justify-content-center btn btn-dark px-5">
+                    Schedule an hour
+                </button>
+            </div>
+            <div>
+                <img
+                    class="rounded-1"
+                    src="https://matheuscostadesign.github.io/nlw6-beauty-salon/assets/images/main.jpg"
+                    alt=""
+                />
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+#container-home {
+    max-width: 70rem;
+}
+
+#apresentation {
+    max-width: 24rem;
+    margin-right: 4rem;
+}
+
+button {
+    height: 3.125rem;
+}
+
+img {
+    max-width: 40rem;
+    height: auto;
+}
+</style>
