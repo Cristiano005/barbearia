415b17fd4971d3ee9f1c3f45605bd9e2d1ac2d1c
Erisvaldo15 <erisvaldosilvadesousa38@gmail.com>
2024-10-07 20:01:09 -0300

    Home Page Admin updated with Date Filter, "Schedule Status" Graphic added and Vue DatePicker Library installed

--- barbershop-admin/src/views/HomeView.vue
+++ barbershop-admin/src/views/HomeView.vue
@@ -1,77 +1,74 @@
 <template>
     <TheHeader />
     <main class="p-5" style="background-color: rgb(244, 247, 254)">
-        <div class="table-header row flex-wrap justify-content-between gap-2">
-            <h2 class="col-12 col-lg-6">Dashboard</h2>
-            <div class="d-flex col-12 col-lg-5 filters flex-wrap gap-3">
-                <div class="col-12 col-lg-5">
-                    <input class="form-control me-2 h-100 p-2" type="search" placeholder="Search by services" aria-label="Search" />
-                </div>
-                <div class="col-12 col-lg-4">
-                    <select class="form-select h-100 p-2" aria-label="Default select example">
-                        <option selected>All</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-                <button type="button" class="btn btn-success col-12 col-lg-2 p-2">+ Add service</button>
+        <div class="table-header row flex-wrap justify-content-between">
+            <h2 class="col-12 col-lg-9">Dashboard</h2>
+            <div class="d-flex col-12 col-lg-3 filters flex-wrap justify-content-end align-items-center gap-3">
+                <VueDatePicker
+                    v-model="dateInterval"
+                    placeholder="Select a date interval"
+                    :type="false"
+                    format="dd/MM/yyyy"
+                    range
+                    multi-calendars />
             </div>
         </div>
-        <div class="row analysys bg-light mt-5 gap-4">
+        <div class="row analysys bg-light mt-5 gap-5">
             <div class="card col-lg-3 mb-3">
                 <div class="row align-items-center card-body text-center">
                     <h4 class="card-title">Total Revenue</h4>
-                    <span class="card-text fs-1">
-                        R$ 10.000,56
-                    </span>
+                    <span class="card-text fs-1"> R$ 10.000,56 </span>
                 </div>
             </div>
             <div class="card col-lg-3 mb-3">
                 <div class="row align-items-center card-body text-center">
                     <h4 class="card-title">Total Scheduled</h4>
-                    <span class="card-text fs-1">
-                        123
-                    </span>
+                    <span class="card-text fs-1"> 158 </span>
                 </div>
             </div>
             <div class="card col-lg-3 mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card-body row align-items-center text-center">
+                    <h4 class="card-title">Hours Worked</h4>
+                    <span class="card-text fs-1"> 152h </span>
                 </div>
             </div>
 
             <div class="card col-lg-2 mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card-body row align-items-center text-center">
+                    <h4 class="card-title">New Customers</h4>
+                    <span class="card-text fs-1"> 12 </span>
                 </div>
             </div>
-            
-            <div class="col-12 col-lg-7" style="height: 28rem;">
-                <div class="chart-container h-100">
-                    <Bar :data="totalRevenue.data" :options="totalRevenue.options" />
-                </div>
+
+            <div class="col-12 col-lg-5" style="height: 28rem">
+                <h2 class="text-center text-dark">Revenue</h2>
+                <Bar :data="totalRevenue.data" :options="totalRevenue.options" />
             </div>
-            <div class="col-lg-4 text-light rounded" style="height: 28rem;">
+            <div class="col-lg-3 text-light rounded" style="height: 28rem">
+                <h2 class="text-center text-dark">Payments</h2>
                 <Doughnut :data="paymentsTypes.data" :options="paymentsTypes.options" />
+            </div>
+            <div class="col-lg-3 text-light rounded" style="height: 28rem">
+                <h2 class="text-center text-dark">Schedules Status</h2>
+                <Doughnut :data="schedulesStatus.data" :options="schedulesStatus.options" />
             </div>
         </div>
     </main>
 </template>
 
 <script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue';
+
 import TheHeader from '@/components/TheHeader.vue';
 
-import { Chart as ChartJS, Title, ArcElement, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 import { Bar, Doughnut } from 'vue-chartjs';
-
+import { Chart as ChartJS, Title, ArcElement, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 import type { ChartData, ChartOptions } from 'chart.js';
 
-import { reactive } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const dateInterval = ref(null);
 
 const totalRevenue = reactive<{
     data: ChartData<'bar'>;
@@ -106,7 +103,7 @@ const totalRevenue = reactive<{
         },
         plugins: {
             legend: {
-                position: 'top' as 'top',
+                position: 'bottom' as 'bottom',
                 labels: {
                     font: {
                         size: 18,
@@ -123,12 +120,12 @@ const paymentsTypes = reactive<{
     options: ChartOptions<'bar'>;
 }>({
     data: {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: ['Credit Card', 'Debit Card', 'Pix', 'Money'],
         datasets: [
             {
                 label: 'My First Dataset',
-                data: [300, 50, 100],
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+                data: [12, 78, 18, 44],
+                backgroundColor: ['#dc3545', '#0d6efd', '#ffc107', '#198754'],
                 hoverOffset: 4
             }
         ]
@@ -150,7 +147,51 @@ const paymentsTypes = reactive<{
         },
         plugins: {
             legend: {
-                position: 'top' as 'top',
+                position: 'bottom' as 'bottom',
+                labels: {
+                    font: {
+                        size: 18,
+                        weight: 'bold'
+                    }
+                }
+            }
+        }
+    }
+});
+
+const schedulesStatus = reactive<{
+    data: ChartData<'bar'>;
+    options: ChartOptions<'bar'>;
+}>({
+    data: {
+        labels: ['Finished', 'Absent', 'Cancelled'],
+        datasets: [
+            {
+                label: 'My First Dataset',
+                data: [140, 5, 13],
+                backgroundColor: ['#198754', '#ffc107', '#dc3545'],
+                hoverOffset: 4
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false // Isso remove as linhas de grade do eixo X
+                }
+            },
+            y: {
+                grid: {
+                    display: false // Isso remove as linhas de grade do eixo X
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'bottom' as 'bottom',
                 labels: {
                     font: {
                         size: 18,
