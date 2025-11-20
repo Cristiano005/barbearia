e7946ab215206ee723bb92d3abfa3213f9eef637
Cristiano005 <cristianosilvadesousa05@gmail.com>
2025-11-20 19:37:28 -0300

    fix(header): replace anchors with router-link and adjust layout for proper SPA navigation

--- barbershop-admin/src/components/TheHeader.vue
+++ barbershop-admin/src/components/TheHeader.vue
@@ -1,59 +1,38 @@
 <template>
-    <header>
-        <nav class="navbar navbar-dark bg-dark p-3">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Barber Shop</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasDarkNavbar"
-                    aria-controls="offcanvasDarkNavbar"
-                    aria-label="Toggle navigation">
+    <header class="p-2 bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand fw-medium" href="/">Barber Shop</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                    aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div
-                    class="offcanvas offcanvas-start text-bg-dark"
-                    tabindex="-1"
-                    id="offcanvasDarkNavbar"
-                    aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                            Menu
-                        </h5>
-                        <button
-                            type="button"
-                            class="btn-close btn-close-white"
-                            data-bs-dismiss="offcanvas"
-                            aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <router-link class="nav-link" to="/">
-                                    Home
-                                </router-link>
-                            </li>
-                            <li class="nav-item">
-                                <router-link class="nav-link" to="/schedules">
-                                    Schedules
-                                </router-link>
-                            </li>
-                            <li>
-                                <router-link class="nav-link" to="/services">
-                                    Services
-                                </router-link>
-                            </li>
-                            <li>
-                                <button class="btn btn-outline-danger" @click="logout">
-                                    Logout
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
+                    <ul class="navbar-nav gap-2">
+                        <li class="nav-item">
+                            <router-link class="nav-link text-white" to="/">Dashboard</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link text-white" to="/schedules">Schedules</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link text-white" to="/services">Services</router-link>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <button class="btn btn-outline-light dropdown-toggle border-0" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <i class="bi bi-person fs-5"></i>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li>
+                                    <button class="dropdown-item" @click="logout">Logout</button>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     </header>
 </template>
 
@@ -67,9 +46,9 @@ async function logout() {
 
     try {
         const { data } = await axiosInstance.post("api/v1/auth/admin/logout", { withCredentials: true });
-        if(data.success) router.push({path: "/signin"});
-    } 
-    
+        if (data.success) router.push({ path: "/signin" });
+    }
+
     catch (error) {
         console.log(error);
     }
@@ -77,3 +56,30 @@ async function logout() {
 }
 
 </script>
+
+<style lang="scss">
+header #navbarContent ul li:nth-last-child(n+1) a {
+    position: relative;
+    top: 0;
+    transition: color 0.25s !important;
+
+    &:hover {
+        color: white !important;
+    }
+
+    &::after {
+        content: '';
+        width: 0%;
+        height: 0.1rem;
+        position: absolute;
+        left: 0;
+        bottom: -0.8rem;
+        transition: width 0.5s !important;
+        background-color: rgb(255, 255, 255) !important; // Ensure visibility
+    }
+
+    &:hover::after {
+        width: 100% !important;
+    }
+}
+</style>
