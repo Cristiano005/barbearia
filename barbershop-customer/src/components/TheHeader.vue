180cf7e0c8c710442a5594881523283a2200e3ca
Cristiano005 <cristianosilvadesousa05@gmail.com>
2025-12-04 03:32:38 -0300

    fix(header): adjust navigation to remove section anchors on profile page for correct routing behavior

--- barbershop-customer/src/components/TheHeader.vue
+++ barbershop-customer/src/components/TheHeader.vue
@@ -2,28 +2,38 @@
     <header class="fixed-top p-2 bg-dark">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand fw-medium" href="#">Barber Shop</a>
+                <RouterLink class="navbar-brand fw-medium" to="/">Barber Shop</RouterLink>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
                     aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
                     <ul class="navbar-nav gap-2">
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="/#home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="/#about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="/#services">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="/#testimonials">Testimonials</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="/#contact">Contact</a>
-                        </li>
+
+                        <template v-if="route.name === 'home'">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="/#home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="/#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="/#services">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="/#testimonials">Testimonials</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="/#contact">Contact</a>
+                            </li>
+                        </template>
+
+                        <template v-else>
+                            <li class="nav-item">
+                                <RouterLink class="nav-link text-white" to="/">Home</RouterLink>
+                            </li>
+                        </template>
+
                         <template v-if="!isChecking">
                             <template v-if="!userStore.getAuthenticatedStatus">
                                 <li class="nav-item">
@@ -62,7 +72,11 @@
 <script setup lang="ts">
 
 import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
 import { useUserStore } from "@/stores/user";
+
+const route = useRoute();
 
 const userStore = useUserStore();
 const isChecking = ref<boolean>(true);
